647bc958824bcbf9f84807f28c12e995170aa94c
Maciej Góralczyk <goralczykmaciek@gmail.com>
2025-03-31 16:44:48 +0200

    Created custom editor for VisualDepletion.

---
+++ Assets/_Project/_Scripts/Editor/Gameplay/Resources/VisualDepletionEditor.cs
@@ -0,0 +1,18 @@
+﻿using FrontierPioneers.Gameplay.Resources;
+using UnityEditor;
+
+namespace FrontierPioneers.Editor.Gameplay.Resources 
+{
+    [CustomEditor(typeof(VisualDepletion))]
+    public class VisualDepletionEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Hierarchy note!" , EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("First child of this object ('chunks') should have mineral mesh renderers as children.");
+        }
+    }
+}
